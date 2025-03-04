eb1fb238d42419de12d9f1b2f5c14efb800a3792
Luis1f2 <flores123ruiz2316@gmail.com>
2025-03-03 20:16:53 -0600

    Correcciion

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ import (
 	userInfra "chat/src/Users/infrastructure"
 	"chat/src/core"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Inicializar la conexión a la base de datos
+
 	db, err := core.GetDBPool()
 	if err != nil {
 		log.Fatalf("Error al conectar a la base de datos: %v", err)
 	}
-	defer db.Close() // Defer justo después de abrir el recurso
+	defer db.Close()
 
-	// Crear el router de Gin
 	router := gin.Default()
+	router.Use(cors.Default())
 
 	userInfra.InitUsers(db, router)
 
 	messageInfra.InitMessages(db, router)
 
-	// Iniciar el servidor en el puerto 8080
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Error al iniciar el servidor: %v", err)
 	}
